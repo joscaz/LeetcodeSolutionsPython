c5f76b7e103349bf480eca26926ce1f789cd1037
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2023-12-16 12:43:06 -0600

    Time: 64 ms (13.04%), Space: 16.9 MB (32.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,19 +3,14 @@ class Solution:
         hm_s = {}
         hm_t = {}
 
-        for letter in s:
-            if letter not in hm_s:
-                hm_s[letter] = 1
-            
-            else:
-                hm_s[letter] += 1
+        for word in s:
+            if word not in hm_s:
+                hm_s[word] = 0
+            hm_s[word] += 1
         
-        for letter in t:
-            if letter not in hm_t:
-                hm_t[letter] = 1
-            
-            else:
-                hm_t[letter] += 1
-        
-        return hm_t == hm_s
-        
+        for word in t:
+            if word not in hm_t:
+                hm_t[word] = 0
+            hm_t[word] += 1
+
+        return hm_s == hm_t
