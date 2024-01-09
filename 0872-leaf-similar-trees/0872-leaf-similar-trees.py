a6f12454905a28a9529c55ef26a774250f6b0198
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-01-09 09:29:03 -0600

    Time: 46 ms (20.67%), Space: 17.4 MB (18.13%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -10,7 +10,7 @@ class Solution:
             if not root: return []
             if not root.left and not root.right:
                 return [root.val]
-            
+
             return dfs(root.left) + dfs(root.right)
-        
+
         return dfs(root1) == dfs(root2)
