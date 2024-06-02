4bcffc830a88b662217c31b0ab7779075d180edc
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-06-02 16:07:17 -0600

    Time: 171 ms (44.84%), Space: 21 MB (32.18%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -6,8 +6,6 @@ class Solution:
         i, j = 0, len(s) - 1
 
         while i < j:
-            temp = s[i]
-            s[i] = s[j]
-            s[j] = temp
+            s[i], s[j] = s[j], s[i]
             i += 1
             j -= 1
