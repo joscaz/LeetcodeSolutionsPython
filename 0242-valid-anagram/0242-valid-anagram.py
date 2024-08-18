ed414dcca843427ebb1caa5e4a921e3b4b2cf319
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-08-17 19:36:18 -0600

    Time: 37 ms (96.16%), Space: 16.9 MB (45.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
         hm_s = {}
         hm_t = {}
-
-        for word in s:
-            if word not in hm_s:
-                hm_s[word] = 0
-            hm_s[word] += 1
+        for let in s:
+            if let not in hm_s:
+                hm_s[let] = 0
+            hm_s[let] += 1
         
-        for word in t:
-            if word not in hm_t:
-                hm_t[word] = 0
-            hm_t[word] += 1
-
-        return hm_s == hm_t
+        for let in t:
+            if let not in hm_t:
+                hm_t[let] = 0
+            hm_t[let] += 1
+        
+        if hm_s == hm_t:
+            return True
+        
+        return False
