77442514a2459ce1dcd5ea5c5d67e56c6971a0cb
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-09-10 19:49:08 -0600

    Time: 33 ms (73.24%), Space: 16.3 MB (93.56%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -3,6 +3,9 @@ class Solution:
         xor = start ^ goal
         flips = 0
 
+        # XOR - this operation will count if a bit is different, meaning there needs
+        # to be a flip. 
+
         for i in range(len(bin(xor))):
             if bin(xor)[i] == '1':
                 flips += 1
