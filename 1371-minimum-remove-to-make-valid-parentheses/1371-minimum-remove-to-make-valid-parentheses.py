8055bc7dbb5073d95ed4bd49d58aa8b92c1d7d8b
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-10-01 14:47:48 -0600

    Time: 71 ms (76.79%), Space: 20.3 MB (5.17%) - LeetHub

---
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        '''
+        lee(t(c)o)de)
+        count = 0   ans = ""
+        -------------
+        count = 2
+        -------------
+        -------------
+        -------------
+        -------------
+        '''
+        ans = []
+        count = 0
+
+        for c in s:
+            if c == '(':
+                count += 1
+                ans.append(c)
+            elif c == ')' and count > 0:
+                    ans.append(c)
+                    count -= 1
+            elif c != ')':
+                ans.append(c)
+        
+        take_out_open = []
+
+        for c in ans[::-1]:
+            if c == '(' and count > 0:
+                count -= 1
+            else:
+                take_out_open.append(c)
+        
+        return ''.join(take_out_open[::-1])
