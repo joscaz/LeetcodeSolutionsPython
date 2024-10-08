7fe54b7e0ee8a2117e97e251e0c36e8df0f0701d
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-10-08 12:23:29 -0600

    Time: 101 ms (18.18%), Space: 22.3 MB (17.47%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,21 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hm = {}
-        for i in range(len(strs)):
-            hm[i] = {}
-            for letter in strs[i]:
-                if letter not in hm[i]:
-                    hm[i][letter] = 0
-                hm[i][letter] += 1
+        res = defaultdict(list)
 
-        ans = []
-        for i in range(len(strs)):
-            curr = []
-            if hm[i] != 0:
-                curr.append(strs[i])
-                for j in range(i+1, len(strs)):
-                    if hm[i] == hm[j] and hm[j] != 0:
-                        curr.append(strs[j])
-                        hm[j] = 0
-                ans.append(curr)
-        return ans
+        for word in strs:
+            count = [0] * 26
+
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            
+            res[tuple(count)].append(word)
+        
+        return res.values()
