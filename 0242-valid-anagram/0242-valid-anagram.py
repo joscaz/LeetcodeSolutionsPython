25277d213652a884bb4f2dc519aeb3f0a778de55
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-10-12 16:14:21 -0600

    Time: 51 ms (46.02%), Space: 16.9 MB (57.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hm1 = {}
-        hm2 = {}
+        hm1 = defaultdict(int)
+        hm2 = defaultdict(int)
 
         for c in s:
-            if c not in hm1:
-                hm1[c] = 0
             hm1[c] += 1
-             # hm1.get(c, 0) + 1
-
         for c in t:
-            if c not in hm2:
-                hm2[c] = 0
             hm2[c] += 1
-            # hm2.get(c, 0) + 1
 
         return hm1 == hm2
