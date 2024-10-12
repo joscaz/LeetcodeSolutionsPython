97ee9ffef9ca92c201399ffb3ab4d86c12db81da
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-10-12 16:15:51 -0600

    Time: 36 ms (96.95%), Space: 17 MB (57.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,9 +3,11 @@ class Solution:
         hm1 = defaultdict(int)
         hm2 = defaultdict(int)
 
-        for c in s:
-            hm1[c] += 1
-        for c in t:
-            hm2[c] += 1
+        if len(s) != len(t):
+            return False
+        
+        for i in range(len(s)):
+            hm1[s[i]] += 1
+            hm2[t[i]] += 1
 
         return hm1 == hm2
