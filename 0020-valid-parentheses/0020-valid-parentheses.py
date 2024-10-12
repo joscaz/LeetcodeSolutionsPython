95a6a6117fc66abe6d11cd6172e60aa224a6ec92
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-10-12 12:28:59 -0600

    Time: 37 ms (44.63%), Space: 16.5 MB (59.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        symb = {'(':')', '[':']', '{':'}'}
-        stack = []
-
-
+        symbols = {'(':')', '[':']', '{':'}'}
+        stack = deque()
+        
         for c in s:
-            if c in symb:
-                stack.append(c)
-            elif stack and c == symb[stack.pop()]:
+            if c in symbols:
+                stack.appendleft(c)
+            elif stack and c == symbols[stack.popleft()]:
                 continue
             else:
                 return False
-        
-        if stack:
-            return False
-        return True
+        return True if not stack else False
+        '''
+        stack = (
+        symbols[]
+        '''
