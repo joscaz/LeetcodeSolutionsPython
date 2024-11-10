acb0ad5c14b18aef925eea15ceb94be4d7692cfa
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-11-10 12:57:36 -0600

    Time: 0 ms (100%), Space: 17.6 MB (81.86%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,6 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low, high = 0, len(nums)-1
+        low, high = 0, len(nums) - 1
 
         while low <= high:
             mid = (low + high) // 2
@@ -11,4 +11,5 @@ class Solution:
                 high = mid - 1
             else:
                 low = mid + 1
+        
         return -1
