4ecd54feec4a50c75dfdfba1900191dd9767c8b1
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-11-15 22:22:55 -0600

    Time: 935 ms (19.99%), Space: 19.7 MB (92.07%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,11 +1,6 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # sorting the array will make it easier
-        # be aware of duplicates
         nums.sort()
-        '''
-        [-4, -1, -1, 0, 1, 2]
-        '''
         ans = []
 
         for i in range(len(nums)-2):
@@ -14,16 +9,16 @@ class Solution:
             j = i + 1
             k = len(nums)-1
             while j < k:
-                cur_triplet = [nums[i], nums[j], nums[k]]
                 op = nums[i] + nums[j] + nums[k]
+                cur_triplet = [nums[i], nums[j], nums[k]]
                 if op == 0:
-                    ans.append(cur_triplet)
                     while j < k and nums[j] == nums[j+1]:
                         j += 1
                     while j < k and nums[k] == nums[k-1]:
                         k -= 1
                     j += 1
                     k -= 1
+                    ans.append(cur_triplet)
                 elif op > 0:
                     k -= 1
                 else:
