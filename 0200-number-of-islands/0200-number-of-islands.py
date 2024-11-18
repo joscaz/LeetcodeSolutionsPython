919f67a8fc2e2d077e50818a040994f00de58888
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-11-18 17:58:47 -0600

    Time: 250 ms (45.25%), Space: 28.1 MB (5.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,16 +6,16 @@ class Solution:
         visited = set()
 
         def dfs(row, col):
-            if row < 0 or row == ROWS or col < 0 or col == COLS or (row, col) in visited or grid[row][col] == '0':
+            if row < 0 or row == ROWS or col < 0 or col == COLS or (row,col) in visited or grid[row][col] == '0':
                 return 0
-            
             visited.add((row, col))
+            
+            return (dfs(row+1,col) + dfs(row-1,col) + dfs(row,col+1) + dfs(row,col-1))
 
-            return (dfs(row+1, col) + dfs(row-1, col) + dfs(row, col+1) + dfs(row, col-1))
-        
         for i in range(ROWS):
             for j in range(COLS):
                 if (i, j) not in visited and grid[i][j] == '1':
-                    islands += 1
                     dfs(i, j)
+                    islands += 1
+
         return islands
