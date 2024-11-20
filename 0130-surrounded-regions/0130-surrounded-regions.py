9f3187d6c47b6674d559a5052ed2e09c4a4c2f75
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-11-19 19:24:34 -0600

    Time: 3 ms (89.85%), Space: 20.8 MB (43.49%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -6,27 +6,27 @@ class Solution:
         ROWS = len(board)
         COLS = len(board[0])
 
-        def dfs(r, c):
+        def capture(r, c):
             if r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != 'O':
                 return
             board[r][c] = 'T'
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
+            capture(r+1, c)
+            capture(r-1, c)
+            capture(r, c+1)
+            capture(r, c-1)
         
+        # Convert all not surrounded regions to T
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] == 'O' and (r in [0, ROWS-1] or c in [0, COLS-1]):
-                    dfs(r,c)
+                    capture(r,c)
         
-        # Change O -> X
+        # Change all surrounded regions to X
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] == 'O':
                     board[r][c] = 'X'
         
-        # Change T -> O        
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] == 'T':
