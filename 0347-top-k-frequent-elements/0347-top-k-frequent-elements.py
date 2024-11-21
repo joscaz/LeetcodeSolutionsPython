61086c948bb6d6e0030e6f3e4b4c3c50745be527
Jose Zertuche <77427867+joscaz@users.noreply.github.com>
2024-11-21 11:21:05 -0600

    Time: 7 ms (54.13%), Space: 20.1 MB (91.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        bucket = [[] for i in range(len(nums)+1)]
+        min_heap = []
         hm = {}
 
         for num in nums:
             if num not in hm:
                 hm[num] = 0
             hm[num] += 1
-
-        for num, freq in hm.items():
-            bucket[freq].append(num)
         
-        top_k = []
-
-        for i in range(len(bucket)-1, -1, -1):
-            for num in bucket[i]:
-                top_k.append(num)
-                k -= 1
-                if k == 0:
-                    return top_k
+        for num, freq in hm.items():
+            heappush(min_heap, (freq, num))
+            if len(min_heap) > k:
+                heappop(min_heap)
+            
+        return [v[1] for v in min_heap]
+        
